afa79296e01fe1d5a5453da7dd34007d848c853f
David R. Smith <codetwiddler@gmail.com>
2024-01-07 18:30:34 -0700

    add label type

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,3 +6,8 @@ interface Album {
   genre: string;
   rating?: number | undefined; //maybe we scale to range of 1-10 someday, amiright?
 }
+
+//Mate our Album props with useful labels using props as keys
+type AlbumLabels = {
+  [Property in keyof Album]: string;
+};
