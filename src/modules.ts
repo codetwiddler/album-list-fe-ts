03eead91a13210523ce15132f9ed62bc4e8e4daf
David R. Smith <codetwiddler@gmail.com>
2024-01-07 18:30:11 -0700

    add 'localization' labels for the album properties

---
+++ src/modules.ts
@@ -0,0 +1,11 @@
+//Labels to match with the props on the Album objects
+//Helps in case we decide to expand the number of properties
+//This methodology could also be used for localization, for instance
+export const albumLabelList: AlbumLabels = {
+  id: "Id:",
+  title: "Title:",
+  artist: "Artist:",
+  releaseYear: "Released:",
+  genre: "Genre:",
+  rating: "Rating:", //Might not use this
+};
