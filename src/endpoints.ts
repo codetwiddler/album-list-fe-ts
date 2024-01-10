9a7398b17699990cac9d1b41c5fd821063c8436a
David R. Smith <codetwiddler@gmail.com>
2024-01-09 21:40:08 -0700

    move fakes to their own file and establish a place to house endpoints

---
+++ src/endpoints.ts
@@ -0,0 +1,8 @@
+const BASE_URL   = "https://localhost:7286/";
+const ALL_ALBUMS = BASE_URL + "Album";
+
+//This constant provides a nice way to aggregate the various endpoints the
+//application is expected to use throughout. Centralizes endpoint setup.
+export const endpoints = {
+    allAlbums: ALL_ALBUMS
+};
