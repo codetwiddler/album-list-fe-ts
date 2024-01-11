1d4f705f62a94ffb29dcfc1ac4cbc00c8b014868
David R. Smith <codetwiddler@gmail.com>
2024-01-10 20:48:50 -0700

    add root to endpoints and change some naming

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,8 +1,10 @@
+const ROOT_PATH  = '/';
 const BASE_URL   = "https://localhost:7286/";
-const ALL_ALBUMS = BASE_URL + "Album";
+const ALBUMS_ALL = BASE_URL + "Album";
 
 //This constant provides a nice way to aggregate the various endpoints the
 //application is expected to use throughout. Centralizes endpoint setup.
 export const endpoints = {
-    allAlbums: ALL_ALBUMS
+    appBase:   ROOT_PATH,
+    allAlbums: ALBUMS_ALL
 };
